d5dcfbc811751dba58d8616a13f9e16f5eb29628
hikaru1337 <hikaryhimon@gmail.com>
2023-12-10 19:08:51 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+
+      boxShadow: {
+        'glow': '0 0 25px #F91A3C, 0 0 50px #F91A3C, 0 0 172px #F91A3C, 0 0 345px #F91A3C, 0 0 600px #F91A3C, 0 0 700px #F91A3C',
+      },
+
+      colors: {
+        black: {
+          100: '#111',
+          200: '#131313',
+          300: '#181818',
+        },
+        white: {
+          100: '#A0A0A0',
+          200: '#E8E8E8'
+        },
+        green: {
+          100: '#52B849'
+        }
+      },
+      fontFamily: {
+        Hanson: ['Hanson-bold', 'sans-serif'],
+        HansonBold: ['Hanson-light', 'sans-serif'],
+        GothamRegular: ['Gotham-regular', 'sans-serif'],
+        GothamBold: ['Gotham-bold', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
